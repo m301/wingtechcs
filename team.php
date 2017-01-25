e1078c1d91bbe4149beb8895d988c5df795957ec
Deeksha Gupta <deekshagupta726@gmail.com>
2017-01-25 22:11:02 +0530

    Add files via upload

--- team.php
+++ team.php
@@ -11,8 +11,11 @@
 <p>
 <hr>
 <!--TEAM PAGE-->
-
-<div class="container">
+<?php include "team_data.php";
+$i=count($team['core']);
+$j=count($team['technical']);
+$k=count($team['management']);
+?>
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="col-lg-12">
@@ -20,67 +23,33 @@
                 <p>WingTech is comprised of ultra-talented and experienced designers, developers and programmers.
                     Learn more about each person by reading their descriptions below:</p>
                 <hr>
-                <div class="row pt-md">
-
-                    <div class="col-md-offset-2 col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+				<div class="row pt-md">
+					<?php
+						$off=floor((12-3*$i)/2);
+						for($l=0;$l<$i;$l++)
+						echo '<div class="col-lg-offset-'.$off.' col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
                         <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
+                            <img src=" '.$team['core'][$l]['photo'].'" class="img-responsive">
                             <ul class="text-center">
-                                <a href="#">
+                                <a href="'.$team['core'][$l]['twt'].'">
                                     <li><i class="fa fa-facebook"></i></li>
                                 </a>
-                                <a href="#">
+                                <a href="'.$team['core'][$l]['twt'].'">
                                     <li><i class="fa fa-twitter"></i></li>
                                 </a>
-                                <a href="#">
+                                <a href="'.$team['core'][$l]['in'].'">
                                     <li><i class="fa fa-linkedin"></i></li>
                                 </a>
                             </ul>
                         </div>
-                        <h1>Shubham Chaubey</h1>
-                        <h2>General Secretary</h2>
-                        <p>IVth Year </p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Assistant General Secretary </h1>
-                        <h2>Assistant General Secretary</h2>
-                        <p>IVth Year</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Treasurer</h1>
-                        <h2>Treasurer</h2>
-                        <p>IVth Year</p>
-                    </div>
+                        <h1>'.$team['core'][$l]['name'].'</h1>
+                        <h2>'.$team['core'][$l]['desig'].'</h2>
+                        <p>'.$team['core'][$l]['year'].' Year</p>
+                    </div>'	
+							
+							
+					?>
+                    
 
                     <!--- for breaking line---->
 
@@ -90,126 +59,33 @@
                     <h3>
                         <ul><b>Technical Team</b></ul>
                     </h3>
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+					<?php
+					for($l=0;$l<$j;$l++)
+					{ 
+						echo '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
                         <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
+                            <img src=" '.$team['technical'][$l]['photo'].'" class="img-responsive">
                             <ul class="text-center">
-                                <a href="#">
+                                <a href="'.$team['technical'][$l]['twt'].'">
                                     <li><i class="fa fa-facebook"></i></li>
                                 </a>
-                                <a href="#">
+                                <a href="'.$team['technical'][$l]['twt'].'">
                                     <li><i class="fa fa-twitter"></i></li>
                                 </a>
-                                <a href="#">
+                                <a href="'.$team['technical'][$l]['in'].'">
                                     <li><i class="fa fa-linkedin"></i></li>
                                 </a>
                             </ul>
                         </div>
-                        <h1>Deeksha</h1>
-                        <h2>Programmer</h2>
-                        <p>IIIrd Year</p>
-                    </div>
-
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Madhurendra</h1>
-                        <h2>Programmer</h2>
-                        <p>IIIrd Year</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Neha</h1>
-                        <h2>Developer</h2>
-                        <p>IIIrd Year</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Yashasvi</h1>
-                        <h2>Programmer</h2>
-                        <p>IInd Year</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Shagufta</h1>
-                        <h2>Programmer</h2>
-                        <p>IInd Year.</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Aakanksha</h1>
-                        <h2>Programmer</h2>
-                        <p>IInd year</p>
-                    </div>
+                        <h1>'.$team['technical'][$l]['name'].'</h1>
+                        <h2>'.$team['technical'][$l]['desig'].'</h2>
+                        <p>'.$team['technical'][$l]['year'].' Year</p>
+                    </div>'	;
+							
+							
+					}
+					?>
+      
 
                     <!--- for breaking line---->
                 </div>
@@ -218,106 +94,29 @@
                     <h3>
                         <ul><b>Management Team</b></ul>
                     </h3>
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
+					<?php
+					for($l=0;$l<$k;$l++)
+						echo '<div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
                         <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
+                            <img src=" '.$team['management'][$l]['photo'].'" class="img-responsive">
                             <ul class="text-center">
-                                <a href="#">
+                                <a href="'.$team['management'][$l]['twt'].'">
                                     <li><i class="fa fa-facebook"></i></li>
                                 </a>
-                                <a href="#">
+                                <a href="'.$team['management'][$l]['twt'].'">
                                     <li><i class="fa fa-twitter"></i></li>
                                 </a>
-                                <a href="#">
+                                <a href="'.$team['management'][$l]['in'].'">
                                     <li><i class="fa fa-linkedin"></i></li>
                                 </a>
                             </ul>
                         </div>
-                        <h1>Gauri</h1>
-                        <h2>Management Head</h2>
-                        <p>IIIrd Year</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Name</h1>
-                        <h2>what they do</h2>
-                        <p>Info/Year</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Name</h1>
-                        <h2>Work</h2>
-                        <p>Information/Year</p>
-                    </div>
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Name</h1>
-                        <h2>Work</h2>
-                        <p>Information/Year</p>
-                    </div>
-
-
-                    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profile">
-                        <div class="img-box">
-                            <img src="assets/images/team-placeholder.jpg" class="img-responsive">
-                            <ul class="text-center">
-                                <a href="#">
-                                    <li><i class="fa fa-facebook"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-twitter"></i></li>
-                                </a>
-                                <a href="#">
-                                    <li><i class="fa fa-linkedin"></i></li>
-                                </a>
-                            </ul>
-                        </div>
-                        <h1>Name</h1>
-                        <h2>Work</h2>
-                        <p>Information/Year</p>
-                    </div>
+                        <h1>'.$team['management'][$l]['name'].'</h1>
+                        <h2>'.$team['management'][$l]['desig'].'</h2>
+                        <p>'.$team['management'][$l]['year'].' Year</p>
+                    </div>'	
+					?>
+                    
 
                 </div>
             </div>
@@ -325,6 +124,8 @@
     </div>
     <br/>
     <?php include "footer.inc"; ?>
+	
+	<script src="assets/js/homepage.js"></script>
 </div>
 
 </body>
